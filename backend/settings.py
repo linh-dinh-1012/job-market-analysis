17040db2001f5503693dda8965e73b5e41e15779
Linh Dinh <dinhlinh1012@gmail.com>
2026-01-27 14:11:12 +0100

    Update settings

--- backend/settings.py
+++ backend/settings.py
@@ -90,18 +90,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 
 DATABASES = {
-  "default": {
-    "ENGINE": "django.db.backends.postgresql",
-    "NAME": DB_NAME,
-    "USER": DB_USER,
-    "PASSWORD": DB_PASSWORD,
-    "HOST": DB_HOST,
-    "PORT": DB_PORT,
-  }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": DB_NAME,
+        "USER": DB_USER,
+        "PASSWORD": DB_PASSWORD,
+        "HOST": DB_HOST,
+        "PORT": DB_PORT,
+        "OPTIONS": {
+            "sslmode": os.getenv("DB_SSLMODE", "require"),
+        },
+    }
 }
 
 # Password validation
