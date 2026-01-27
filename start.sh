129ac9d2eae8591daa3ce6e97d162ac282b5580b
Linh Dinh <dinhlinh1012@gmail.com>
2026-01-27 15:53:34 +0100

    Comment superadmin

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ set -e
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py shell -c "from backend.create_superuser import run; run()"
+# python manage.py shell -c "from backend.create_superuser import run; run()"
 
 gunicorn backend.wsgi:application --bind 0.0.0.0:10000
